3125a37ebd2addba189a94aa651278ba008f2814
Russell Gurdov <f1ash.tm1990@gmail.com>
2022-07-06 00:15:55 -0400

    test Myrat file

---
+++ test.rb
@@ -0,0 +1,13 @@
+# require 'file'
+require 'faker'
+require 'open-uri'
+
+
+# puts URI.open(Faker::Avatar.image)
+def image_fetcher
+  URI.open(Faker::Avatar.image+"dsadas")
+  rescue
+  URI.open("https://robohash.org/sitsequiquia.png?size=300x300&set=set1")
+end
+
+puts image_fetcher.to_path
